f61b0090c0f42c4d98c1781eb8d22926f3bb5bd1
Duong Duy Binh <duongduybinh.dev@gmail.com>
2025-10-28 16:28:45 +0700

    update

--- run/runner.js
+++ run/runner.js
@@ -1,7 +1,28 @@
+var __create = Object.create;
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
 var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getProtoOf = Object.getPrototypeOf;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
 var __commonJS = (cb, mod) => function __require() {
   return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
 };
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
+  // If the importer is in node compatibility mode or this is not an ESM
+  // file that has been converted to a CommonJS file using a Babel-
+  // compatible transform (i.e. "__esModule" has not been set), then set
+  // "default" to the CommonJS "module.exports" for node compatibility.
+  isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
+  mod
+));
 
 // spawn-runner.js
 var require_spawn_runner = __commonJS({
@@ -34,6 +55,7 @@ var require_spawn_runner = __commonJS({
 });
 
 // runner.js
+var import_fs = __toESM(require("fs"));
 var path = require("path");
 var { runCommand, parseBoolean } = require_spawn_runner();
 var args = process.argv.slice(2);
@@ -115,13 +137,13 @@ var FILE_PATH = `/app/data/${normalizedProduct}/schedule/data.csv`;
   if (funcName === "robot") {
     if (isSchedule && isManual) {
       console.log(`Run with mode: Schedule & Manual`);
-      if (fs.existsSync(FILE_PATH)) {
-        if (!fs.existsSync(safeOutputDir)) {
-          fs.mkdirSync(safeOutputDir, { recursive: true });
+      if (import_fs.default.existsSync(FILE_PATH)) {
+        if (!import_fs.default.existsSync(safeOutputDir)) {
+          import_fs.default.mkdirSync(safeOutputDir, { recursive: true });
         }
         const destPath = path.join(dataFilePath, path.basename(FILE_PATH));
         console.log(`Move file ${FILE_PATH} --> ${destPath}`);
-        fs.renameSync(FILE_PATH, destPath);
+        import_fs.default.renameSync(FILE_PATH, destPath);
         const exitCodeGen = await runCommand("node", [
           path.join(__dirname, "csv2json.js"),
           "--product",
