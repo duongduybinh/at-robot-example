a0530ec5fa459c0b8e0374c28ab3989680328729
Dương Duy Bình <9396567+duongduybinh@users.noreply.github.com>
2025-11-13 18:41:31 +0700

    Refactor argument parsing and improve logging

--- run/src/robot.js
+++ run/src/robot.js
@@ -1,28 +1,39 @@
-const { runCommand } = require('./spawn-runner'); // import thư viện tổng quát
+const { runCommand, extractQuotedValue } = require('./spawn-runner'); // import thư viện tổng quát
 
 // Lấy tất cả tham số sau node robot.js
 const args = process.argv.slice(2);
-let testPath = "";
-const childArgs = [];
 
+var testPath = "";
+var childArgs = [];
 for (let i = 0; i < args.length; i++) {
   const arg = args[i];
-  if (arg.startsWith("--testpath=")) testPath = arg.split("=")[1];
-  else if (arg === "--testpath") testPath = args[++i];
+  if (arg.startsWith("--testpath=")) {
+    testPath = extractQuotedValue(arg.split("=")[1]);
+  } else if (arg === "--testpath") {
+    testPath = extractQuotedValue(args[++i]);
+  }
   else childArgs.push(arg);
+
 }
 
+console.log("[robot.js] Argument Summary ==============");
+console.log("Test Path:", testPath || "(default)");
+console.log("Extra Args:", childArgs.length ? childArgs : "(none)");
+console.log("===========================================");
+
 if (args.length === 0) {
-  console.error('Please provide Robot Framework arguments.');
+  console.error("Please provide Robot Framework arguments.");
   process.exit(1);
 }
 
 (async () => {
-  // Chạy Robot Framework, log trực tiếp (thụt vào 2 space)
-  const exitCode = await runCommand('robot', [testPath, ...args],
+  const exitCode = await runCommand(
+    "robot",
+    [...childArgs, testPath],
     {
       cwd: __dirname,
-      stdio: "inherit",
-    });
+      stdio: "inherit"
+    }
+  );
   process.exit(exitCode);
 })();
